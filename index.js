1233c63555c37496cac4ca5e53100ee28f63cd4e
madymona <madymona@gmail.com>
2024-07-11 20:41:10 -0400

    pug files, public directory  created

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
-
 const app = express();
-const PORT = 3000;
 
-
-
-// Middleware
+app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Custom middleware to log request data
+// Middleware to log request data
 app.use((req, res, next) => {
-  console.log(`${req.method} request for '${req.url}'`);
+  console.log(`${req.method} ${req.url}`);
   next();
+});
+
+// Serve static files
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.post('/submit-form', (req, res) => {
+  console.log(req.body);
+  res.send('Form submitted successfully');
+});
+
+app.get('/download/:filename', (req, res) => {
+  const filename = req.params.filename;
+  res.download(`public/images/${filename}`);
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
